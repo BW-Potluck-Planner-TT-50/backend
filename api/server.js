bf293c732703be5ab7531bf7afeda68278406f41
Erica Girges <ericafiorenza@gmail.com>
2020-10-18 13:02:43 -0700

    deploying to heroku

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const server = express();
 server.use(express.json());
 
 server.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+  res.status(200).json({ api: "Potluck Planner" });
 });
 
 module.exports = server;
