e6c8115e8ed74e31591e61b4d74c48e5f7f3ac74
Erica Girges <ericafiorenza@gmail.com>
2020-10-20 14:01:11 -0700

    fix typo

--- database/seeds/003-guests.js
+++ database/seeds/003-guests.js
@@ -1,5 +1,5 @@
 exports.seed = function(knex, Promise) {
   return knex('guests').insert([
-    { name: `50_Cent`, RSVP: true, event_id: 1 },
+    { name: `50_Cent`, rsvp: true, event_id: 1 },
   ]);
 };
